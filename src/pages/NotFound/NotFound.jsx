d2a36be29a4ad7f1b5c291ef3b8a31eba52af779
Safarmurod999 <supermen0904@gmail.com>
2025-01-26 23:48:05 +0500

    notfound page added

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import { Link, useLocation } from 'react-router'
+import notfound from '../../assets/videos/notfound.gif'
 const NotFound = () => {
+  let route = useLocation();
+
   return (
-    <div>NotFound</div>
-  )
+    <section className="not-found">
+      <div className="container">
+        <div className="not-found__title">
+          404
+        </div>
+        <img src={notfound} alt="animation" />
+        <div className="h1">
+          Такой страницы не существует.
+        </div>
+        {route.pathname.startsWith("/dashboard") ? (
+          <Link
+            to={"/admin"}
+          >
+            Главная страница
+          </Link>
+        ) : (
+          <Link
+            to={"/"}
+          >
+            Главная страница
+          </Link>
+        )}
+      </div>
+    </section>)
 }
 
 export default NotFound
